4bef12671ca26895a2cc4ea0177cacc63bf88f6f
edwin.webb <edwin.webb@gmail.com>
2015-10-05 09:24:48 +0100

    Update template

--- templates/displayobject.js
+++ templates/displayobject.js
@@ -1,13 +1,13 @@
 /**
  * {%=o.description%}
  *
- * @exports PIXI.{%=o.exports%}
+ * @exports {%=o.exports%}
  * @extends {%=o.extends%}
  */
 {% if (o.extendpixi) { %}
 import {{%=o.extends%}} from 'pixi.js';
 {% } else { %}
-import {%=o.extends%} from './{{%=o.extends%}}/{{%=o.extends%}}.js';
+import {%=o.extends%} from '../{%=o.extends%}/{%=o.extends%}.js';
 {% } %}
 
 export default class {%=o.exports%} extends {%=o.extends%} {
