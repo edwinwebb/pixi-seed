32e6420e7b4b0705dc83a0db274a0200550a51a2
edwin.webb <edwin.webb@gmail.com>
2016-01-29 11:30:13 +0000

    Attempt to resolve issues with FS etc

--- webpack.config.js
+++ webpack.config.js
@@ -26,11 +26,10 @@ module.exports = {
           { test: /\.html$/, exclude: /node_modules/, loader: "file-loader?name=[path][name].[ext]"},
           { test: /\.jpe?g$|\.svg$|\.png$/, exclude: /node_modules/, loader: "file-loader?name=[path][name].[ext]"},
           { test: /\.json$/, exclude: /node_modules/, loader: "json"},
-          {
-          test: /\.json$/,
-          include: path.join(__dirname, 'node_modules', 'pixi.js'),
-          loader: 'json',
-          }
-        ]
+        ],
+	postLoaders: [{
+          include: path.resolve(__dirname, 'node_modules/pixi.js'),
+          loader: 'transform?brfs'
+        }]
     }
 };
