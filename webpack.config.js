d4c3bcda6cab6b74b8bbe986228f5008029bd55e
edwin.webb <edwin.webb@gmail.com>
2016-02-02 12:14:42 +0000

    Add the JSON loader for PIXIjs back in

--- webpack.config.js
+++ webpack.config.js
@@ -26,8 +26,9 @@ module.exports = {
           { test: /\.html$/, exclude: /node_modules/, loader: "file-loader?name=[path][name].[ext]"},
           { test: /\.jpe?g$|\.svg$|\.png$/, exclude: /node_modules/, loader: "file-loader?name=[path][name].[ext]"},
           { test: /\.json$/, exclude: /node_modules/, loader: "json"},
+          { test: /\.json$/, include: path.join(__dirname, 'node_modules', 'pixi.js'),loader: 'json'}
         ],
-	postLoaders: [{
+	      postLoaders: [{
           include: path.resolve(__dirname, 'node_modules/pixi.js'),
           loader: 'transform?brfs'
         }]
