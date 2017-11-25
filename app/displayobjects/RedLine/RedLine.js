988fe44a98c0892ed4151065e50193592d87ea76
edwin.webb <edwin.webb@gmail.com>
2017-11-25 01:45:48 +0000

    add random number gen

--- app/displayobjects/RedLine/RedLine.js
+++ app/displayobjects/RedLine/RedLine.js
@@ -1,6 +1,8 @@
 /**
  * Red line gfx
  *
+ * Uses Tweening
+ *
  * @exports RedLine
  * @extends Sprite
  */
@@ -8,21 +10,20 @@
 import { Sprite, Texture } from 'pixi.js';
 import LINE from './line.png';
 import { Tween, Easing } from 'es6-tween';
+import { randomRange } from '../../utils';
 
 export default class RedLine extends Sprite {
   constructor(x, y) {
     const texture = Texture.fromImage(LINE);
-    const offset = Math.random() * 1000 - 500;
+    const offset = randomRange(-500, 500);
     super(texture);
     this.alpha = 0.333;
-    this.position.set(x, y);
+    this.position.set(x, randomRange(y - 100, y + 200));
     new Tween(this.position)
-      .to({ y: y + offset }, 40000)
+      .to({ y: y + offset }, randomRange(20000, 40000))
       .repeat(Infinity)
       .yoyo(true)
       .easing(Easing.Quadratic.InOut)
       .start();
   }
-
-  update() {}
 }
